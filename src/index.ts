09620e1e7b2ce2525a4d99c4c78d0f683717a2bb
k4itrun <truchorko2222@gmail.com>
2023-06-30 10:55:01 -0500

    load

--- src/index.ts
+++ src/index.ts
@@ -24,42 +24,42 @@ app.get("/", (req, res) => {
 });
 
 app.get("/tokens", (req, res) => {
-  fs.readFile("./tokens.txt", "utf8", (err, data) => {
+  fs.readFile("./tokens.txt", "utf8", (err, d) => {
     if (err) {
       console.error("Error al leer el archivo:", err);
       res.status(500).send("Error al leer los tokens");
-    } else {
-      res.send(data);
     }
+    res.set("Content-Type", "text/plain");
+    res.send(d);
   });
 });
 
 app.listen(port, () => {
-  console.log(`El servidor está corriendo en el puerto ${port}`);
+  console.log(`Website in localhost:${port}`);
 });
 
 sharedClient.client.on("ready", async () => {
-  console.log(`Anti-cash is complete.`);
+  console.log(`Anticrashing in localhost:${port}`);
   process.on('unhandledRejection', (reason: any, p: Promise<any>) => {
-    console.log('\n\n\n\n\n[🚩 ANTI-CASH] unhandled Rejection:'.toUpperCase());
+    console.log('\n\n\n\n\n=== [unhandled Rejection] ==='.toUpperCase());
     console.log(reason.stack ? String(reason.stack) : String(reason));
     console.log('=== unhandled Rejection ===\n\n\n\n\n'.toUpperCase());
   });
   process.on("uncaughtException", (err: Error, origin: NodeJS.UncaughtExceptionOrigin) => {
-    console.log('\n\n\n\n\n\n[🚩 ANTI-CASH] uncaught Exception'.toUpperCase());
+    console.log('\n\n\n\n\n\n=== [uncaught Exception] ==='.toUpperCase());
     console.log(err.stack ? err.stack : err)
     console.log('=== uncaught Exception ===\n\n\n\n\n'.toUpperCase());
   })
   process.on('uncaughtExceptionMonitor', (err: Error, origin: NodeJS.UncaughtExceptionOrigin) => {
-    console.log('[🚩 ANTI-CASH] uncaught Exception Monitor'.toUpperCase());
+    console.log('=== [uncaught Exception Monitor] ==='.toUpperCase());
   });
   process.on('exit', (code: number) => {
-    console.log('\n\n\n\n\n[🚩 ANTI-CASH] exit'.toUpperCase());
+    console.log('\n\n\n\n\n=== [exit] ==='.toUpperCase());
     console.log(code.toString());
     console.log('=== exit ===\n\n\n\n\n'.toUpperCase());
   });
   process.on('multipleResolves', (type: string, promise: Promise<any>, reason: any) => {
-    console.log('\n\n\n\n\n[🚩 ANTI-CASH] multiple Resolves'.toUpperCase());
+    console.log('\n\n\n\n\n=== [multiple Resolves] ==='.toUpperCase());
     console.log(type, promise, reason);
     console.log('=== multiple Resolves ===\n\n\n\n\n'.toUpperCase());
   });
